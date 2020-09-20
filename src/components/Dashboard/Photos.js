4e8e6db328dc940d8c34b7393ced865321a95c43
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-09-20 09:32:47 -0700

    Nolan added components

---
+++ src/components/Dashboard/Photos.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Photos.scss'
+
+class Photos extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        super(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Photos
---
+++ src/components/FrontPage/Login.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Login.scss'
+
+class Login extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Login
