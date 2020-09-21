807cde076a1e09f08cc1373cf9915f243b1138ce
Darryl King <darrylking15@gmail.com>
2020-09-21 13:13:03 -0400

    s3 file upload endpoint inplemented

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
+const multer = require('multer'); 
+const AWS = require('aws-sdk')
+const { v4: uuidv4 } = require('uuid');
 
 // Controllers
 const authCtrl = require('./controllers/authController');
 const credCtrl = require('./controllers/credController');
 const imageCtrl = require('./controllers/imageController');
+const { Console } = require('console');
 
 // Server Setup -------------------------------------------------------------------
 const app = express();
@@ -61,6 +65,38 @@ massive({
 
 // Need to add ability to send info to Amazon S3 and Rekognition API
 
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET
+}); 
+
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+        callback(null, '')
+    }
+}); 
+
+const upload = multer({storage}).single('image');
+
+app.post('/upload',upload,(req, res) => {
+
+    let myFile = req.file.originalname.split(".")
+    const fileType = myFile[myFile.length - 1]
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuidv4()}.${fileType}`,
+        Body: req.file.buffer
+    }
+        console.log(req.file)
+    s3.upload(params, (error, data) => {
+        if(error){
+            res.status(500).send(error)
+        }
+
+        res.status(200).send(data)
+    })
+})
 
 // Server Run and 
 app.listen(SERVER_PORT, () => {
