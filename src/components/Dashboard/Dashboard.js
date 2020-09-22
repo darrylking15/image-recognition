ad10f4fa426baa21a230b6e4aea88f0cd3672dc5
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-09-21 23:39:34 -0700

    dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Credentials from './Credentials'
 import Photos from './Photos'
 import './Dashboard.scss'
@@ -8,14 +9,53 @@ class Dashboard extends Component{
         super()
 
         this.state = {
-
+            title: '',
+            email: '',
+            password: '',
+            message: '',
+            keyChains: []
         }
     }
 
-    render(){
+        getKeyChains = () => {
+            axios.get('/').then(items => {
+                this.setState({keyChain: keyChain.data})
+        })
+    }
+
+    editKeyChain = (title, email, password, message, id) => {
+        axios.put(`/${id}`, {title, email, password, message, id}).then(() => {
+        })
+    }
+
+    deleteKeyChain = (id) => {
+        axios.delete(`/${id}`).then(() => {
+            this.getKeyChains
+        })
+    }
+
+
+
+        render(){
+            const newKeyChain = this.state.keyChains.map((e, i) => {
+
+                return(
+                    <div key={e.keyChain_id}>
+                        <div className="dashboard--nav">
+
+                        </div>
+                        <div className="keyChain">
+                            <p className="keyChain__title">{e.keyChain.title}</p>
+                            <p className="keyChain__email">{e.keyChain.email}</p>
+                            <p className="keyChain__password">{e.keyChain.password}</p>
+                            <p className="keyChain__message">{e.keyChain.message}</p>
+                        </div>
+                </div>
+            )
+        })
         return(
             <div>
-
+                {newKeyChain}
             </div>
         )
     }
