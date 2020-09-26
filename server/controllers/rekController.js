ecca1c1cec994812fbdb809edc290328039c0947
Darryl King <darrylking15@gmail.com>
2020-09-26 13:39:33 -0400

    merged

--- server/controllers/rekController.js
+++ server/controllers/rekController.js
@@ -105,14 +105,6 @@ module.exports = {
                 if(error){
                     res.status(500).send(error) 
                 }
-<<<<<<< HEAD
-        
-                res.status(200).send(data)
-                
-            })
-           
-           
-=======
                 //Save to Database
                 const db = req.app.get('db');
                 const {ETag, Location, Key, Bucket} = data;
@@ -125,7 +117,7 @@ module.exports = {
                     imgId: imgKey[0].img_id,
                     timestamp: timestamp
                  })
-            })  
+            })     
         },
 
         setProfileImage: async (req, res) => {
@@ -136,8 +128,7 @@ module.exports = {
             console.log("New Profile ID: ", userProfileId);
             updatedUser = await db.set_user_profile( [ userId, userProfileId[0].profile_id, userId ] )  
             console.log("Updated User", updatedUser);
-            res.status(200).send("added profile pic")
->>>>>>> 0210d1685a0a75a8aca553c8178308f749e770f3
+            res.status(200).send("added profile pic");
         }
            
            
