689d8877d58da63076d0d7134ef1d516ca3658eb
Darryl King <darrylking15@gmail.com>
2020-09-26 17:46:14 -0400

    sat

--- server/controllers/rekController.js
+++ server/controllers/rekController.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const AWS = require('aws-sdk')
 const { v4: uuidv4 } = require('uuid');
-//const creds = require('./creds.json')
-
-//const { source, target } = require('./photoData');
 
 
 //const { AWS_ID, AWS_SECRET, AWS_BUCKET_NAME, AWS_COLLECTION_ID, AWS_REGION } = process.env;
@@ -23,7 +20,7 @@ module.exports = {
             region: process.env.AWS_REGION
         })
       
-        const client = new AWS.Rekognition();
+        const rekognition = new AWS.Rekognition();
         
 
         const params = {
@@ -41,7 +38,7 @@ module.exports = {
             },
             SimilarityThreshold: 70
         }
-        client.compareFaces(params, function(err, response) {
+        rekognition.compareFaces(params, function(err, response) {
             if (err) {
             console.log(err, err.stack); // an error occurred
             } else {
@@ -54,31 +51,37 @@ module.exports = {
         });
     } , 
 
-    indexFaces: (req, res) => {
+    indexFaces:  (req, res) => {
         const rekognition = new AWS.Rekognition();
-        const config = new AWS.Config({
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            region: process.env.AWS_REGION
-        })
+        
+        const {faceKey, userId} = req.body; 
         var params = {
-            CollectionId: "imagerekfaces", 
-            DetectionAttributes: [
-            ], 
-            
+            CollectionId: "imagerekfaces",  
             Image: {
              S3Object: {
-              Bucket: process.env.AWS_BUCKET_NAME, 
-              Name: "download.jpeg"
+              Bucket: 'imagerek2020', 
+              Name: faceKey
              }
-            } 
+            }
            };
-           rekognition.indexFaces(params, function(err, data) {
-             if (err) console.log(err, err.stack); // an error occurred
-             else     console.log(data);           // successful response
            
-            }) // for response.faceDetails
-        } , 
+           rekognition.indexFaces(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+          
+           })
+           const db = req.app.get('db');
+           const {FaceRecords} = data;
+    //        const imgKey = await db.post_image( [ userId, timestamp, Location, Bucket, Key, timestamp ] )
+    //        console.log("ImageKey: ", imgKey);
+
+    //        res.status(200).send({ 
+    //            imageInfo: data,
+    //            imgId: imgKey[0].img_id,
+    //            timestamp: timestamp
+    }
+
+, 
         
         upload64S3: (req, res) => {
             const s3 = new AWS.Config({
@@ -109,7 +112,7 @@ module.exports = {
                 const db = req.app.get('db');
                 const {ETag, Location, Key, Bucket} = data;
                 const timestamp = Date.now();
-                imgKey = await db.post_image( [ userId, timestamp, Location, Bucket, Key, timestamp ] )
+                const imgKey = await db.post_image( [ userId, timestamp, Location, Bucket, Key, timestamp ] )
                 console.log("ImageKey: ", imgKey);
 
                 res.status(200).send({ 
@@ -124,31 +127,14 @@ module.exports = {
             console.log('Set Profile Image Called');
             const db = req.app.get('db');
             const { userId, imgId } = req.body; 
-            userProfileId = await db.set_profile_img( [ userId, imgId ] )
+            const userProfileId = await db.set_profile_img( [ userId, imgId ] ); 
             console.log("New Profile ID: ", userProfileId);
-            updatedUser = await db.set_user_profile( [ userId, userProfileId[0].profile_id, userId ] )  
+            const updatedUser = await db.set_user_profile( [ userId, userProfileId[0].profile_id, userId ] )  
             console.log("Updated User", updatedUser);
             res.status(200).send("added profile pic");
         }
-           
-           
-           
-           
-           
-           
-            // s3Bucket.putObject(data, function(err, data){
-            //         console.log("put object ", res.data)
-            //         if (err) { 
-            //             console.log(err);
-            //             console.log("the data", data)
-            //             console.log('Error uploading data: ', data); 
-            //         } else {
-            //             console.log('succesfully uploaded the image!');
-                        
-            //         }
-            //     });
-            //             }
-                        
-        
-}
-    
+
+
+
+    }
+       
